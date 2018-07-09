100bf08cbcc403b6746018527a313be99ed1514c
RuslanBarcho <RRCFoxyLoxy@yandex.ru>
2018-07-09 18:41:40 +0300

    update

--- src/sample/Database/DBWrapper.java
+++ src/sample/Database/DBWrapper.java
@@ -24,7 +24,7 @@ public class DBWrapper {
             System.out.println("ERROR (db connect) "+ r.toString());
         }
     }
-    
+
     //void to add new record
     public void setField(GroupModel var){
         try{
